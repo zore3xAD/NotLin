fdf353147ee5096726becaadc4b31b8893cf1d28
Anton Dobrohorskyi <zore3x@gmail.com>
2018-06-06 16:41:19 +0300

    Add Contracts

---
+++ app/src/main/java/com/android/zore3x/notlin/contracts/NoteContract.kt
@@ -0,0 +1,17 @@
+package com.android.zore3x.notlin.contracts
+
+import com.android.zore3x.notlin.data.Note
+
+interface NoteContract {
+
+    interface OnLoadCallback {
+        fun onComplete(data: MutableList<Note>)
+        fun onError(message: String)
+    }
+
+    interface OnInsertCallback {
+        fun onComplete(id: Long)
+        fun onError(message: String)
+    }
+
+}
